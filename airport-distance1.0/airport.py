963daaa02a1c67e083876179005996e7184af7c8
achac <1753407661@qq.com>
2017-06-30 13:54:07 +0800

    Update airport.py

--- airport-distance1.0/airport.py
+++ airport-distance1.0/airport.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-def search4prepositions_zhs(phrase: str) -> set:
+def search4prepositions_zhs(phrase: str) -> set:#定义函数
     """Return any prepositions found in a supplied phrase."""
     # Source 
     # Source 
     PREPOSITION_zhs = set('')
     return PREPOSITION_zhs.intersection(set(phrase))
 
-def search4letters(phrase: str, letters: str='') -> set:
+def search4letters(phrase: str, letters: str='') -> set:#定义函数
     """Return a set of the 'letters' found in 'phrase'."""
     return set(letters).intersection(set(phrase))
